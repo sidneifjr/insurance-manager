e56353155727bd9caafbf745b4457349d97310c0
Sidnei Farias <sidneijr.77@gmail.com>
2024-06-24 15:07:20 -0300

    feat: update table values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,96 +1,79 @@
-import { Paragraph } from '@/components/typography/paragraph'
+import { getContent } from '@/actions/getContent'
+import { H2 } from '@/components/typography/h2'
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+import { writeDataToDatabase } from '@/modules/writeDataToDatabase'
 
-const invoices = [
-  {
-    invoice: 'INV001',
-    paymentStatus: 'Paid',
-    totalAmount: '$250.00',
-    paymentMethod: 'Credit Card',
-  },
-  {
-    invoice: 'INV002',
-    paymentStatus: 'Pending',
-    totalAmount: '$150.00',
-    paymentMethod: 'PayPal',
-  },
-  {
-    invoice: 'INV003',
-    paymentStatus: 'Unpaid',
-    totalAmount: '$350.00',
-    paymentMethod: 'Bank Transfer',
-  },
-  {
-    invoice: 'INV004',
-    paymentStatus: 'Paid',
-    totalAmount: '$450.00',
-    paymentMethod: 'Credit Card',
-  },
-  {
-    invoice: 'INV005',
-    paymentStatus: 'Paid',
-    totalAmount: '$550.00',
-    paymentMethod: 'PayPal',
-  },
-  {
-    invoice: 'INV006',
-    paymentStatus: 'Pending',
-    totalAmount: '$200.00',
-    paymentMethod: 'Bank Transfer',
-  },
-  {
-    invoice: 'INV007',
-    paymentStatus: 'Unpaid',
-    totalAmount: '$300.00',
-    paymentMethod: 'Credit Card',
-  },
-]
+interface Coberturas {
+  nome: string
+  valor: number
+}
 
-export default function Home() {
+interface Segurado {
+  nome: string
+  email: string
+  cpfCnpj: string
+}
+
+interface User {
+  id: number
+  numero: number
+  valorPremio: number
+  segurado: Segurado
+  coberturas: Coberturas[]
+}
+
+export default async function Home() {
+  const data = (await getContent()) as User[]
+  console.log(data)
+
+  await writeDataToDatabase()
+
   return (
     <>
-      <Paragraph>Home</Paragraph>
+      <H2>Visão Geral</H2>
 
       <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
-
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
+            <TableHead>Número</TableHead>
+            <TableHead>Valor Prêmio</TableHead>
+            <TableHead>Nome</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>CPF/CNPJ</TableHead>
+            <TableHead>Coberturas Nome</TableHead>
+            <TableHead>Coberturas Valor</TableHead>
           </TableRow>
         </TableHeader>
 
         <TableBody>
-          {invoices.map((invoice) => (
-            <TableRow key={invoice.invoice}>
-              <TableCell className="font-medium">{invoice.invoice}</TableCell>
-              <TableCell>{invoice.paymentStatus}</TableCell>
-              <TableCell>{invoice.paymentMethod}</TableCell>
-              <TableCell className="text-right">
-                {invoice.totalAmount}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
+          {data.map((item) => {
+            return (
+              <TableRow key={item.id}>
+                <TableCell className="font-medium">{item.numero}</TableCell>
+                <TableCell>{item.valorPremio}</TableCell>
+                <TableCell>{item.segurado.nome}</TableCell>
+                <TableCell>{item.segurado.email}</TableCell>
+                <TableCell>{item.segurado.cpfCnpj}</TableCell>
 
-        <TableFooter>
-          <TableRow>
-            <TableCell colSpan={3}>Total</TableCell>
-            <TableCell className="text-right">$2,500.00</TableCell>
-          </TableRow>
-        </TableFooter>
+                {item.coberturas.map((cobertura) => {
+                  return (
+                    <>
+                      <TableCell>{cobertura.nome}</TableCell>
+                      <TableCell>{cobertura.valor}</TableCell>
+                    </>
+                  )
+                })}
+              </TableRow>
+            )
+          })}
+        </TableBody>
       </Table>
     </>
   )
