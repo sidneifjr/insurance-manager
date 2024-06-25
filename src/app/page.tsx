d05edda21e6121a2153e9c63de87d93f43b95932
Sidnei Farias <sidneijr.77@gmail.com>
2024-06-25 18:56:57 -0300

    refactor(dashboard): update styles + function naming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,23 @@ import { DashboardTable } from '@/components/dashboard/dashboard-table'
 import { ModalForm } from '@/components/modal/modal-form'
 import { H2 } from '@/components/typography/h2'
 import { Paragraph } from '@/components/typography/paragraph'
-import { writeDataToDatabase } from '@/modules/writeDataToDatabase'
+import { writeDataToDB } from '@/modules/writeDataToDB'
 
 export default async function Home() {
-  await writeDataToDatabase()
+  await writeDataToDB()
 
   const data = await getItems()
 
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex flex-col gap-1">
-        <H2>Visão Geral</H2>
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-1">
+          <H2>Visão Geral</H2>
 
-        <Paragraph>
-          Confira os dados mais importantes dos assegurados cadastrados.
-        </Paragraph>
+          <Paragraph>
+            Confira os dados mais importantes dos assegurados cadastrados.
+          </Paragraph>
+        </div>
 
         <ModalForm />
       </div>
