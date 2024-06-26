a39291ba976b22387e0d1301f4cacf390fad37b9
Sidnei Farias <sidneijr.77@gmail.com>
2024-06-26 12:37:47 -0300

    fix(api): disable cache on GET request

--- src/api/getItems.ts
+++ src/api/getItems.ts
@@ -2,7 +2,7 @@ import { api } from '@/services/api'
 import { User } from '@/types/user'
 
 export async function getItems() {
-  const content: Promise<User[]> = await api('/content')
+  const content: Promise<User[]> = await api('/content', { cache: 'no-store' })
 
   return content
 }
