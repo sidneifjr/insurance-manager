2f9f85a3a745f7fa3a7b5aeea35f4b606b1164dc
Sidnei Farias <sidneijr.77@gmail.com>
2024-06-27 21:08:35 -0300

    refactor(header): update styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ import { H1 } from './typography/h1'
 export function Header() {
   return (
     <header
-      className="flex h-20 items-center border-b border-b-gray-400 bg-gray-700 px-8"
+      className="flex h-20 items-center border-b border-b-gray-400 bg-slate-900 px-8"
       data-testid="header"
     >
       <div className="flex w-full items-center justify-between gap-12">
